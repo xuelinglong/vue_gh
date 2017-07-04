7c4daf077f9de28b1becea4b80239b8cdf4e9adb
WangXueLing <921223971@qq.com>
2017-07-04 11:12:21 +0800

    导航栏改为使用数组

--- gank-wxl/src/App.vue
+++ gank-wxl/src/App.vue
@@ -1,44 +1,7 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab border-1px">
-
-      <div class="tab-item">
-        <!--使用指令 router-link 组件来导航 -->
-        <!-- 通过 to 属性指定链接 -->
-        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-        <router-link to="/welfare">福利</router-link>
-      </div>   
-
-      <div class="tab-item">
-        <router-link to="/android">安卓</router-link>
-      </div>
-
-      <div class="tab-item">
-        <router-link to="/ios">IOS</router-link>
-      </div>
-
-      <div class="tab-item">
-        <router-link to="/web">前端</router-link>
-      </div>
-
-      <div class="tab-item">
-        <router-link to="/rest">休息视频</router-link>
-      </div>
-
-      <!--<div class="tab-item">
-        APP
-      </div>
-
-      <div class="tab-item">
-        瞎推荐
-      </div>
-
-      <div class="tab-item">
-        拓展资源
-      </div>-->
-
-    </div>
+    <v-menu></v-menu> 
     <router-view></router-view>
   </div>
 </template>
@@ -46,37 +9,17 @@
 <script>
   // 引用
   import header from './components/header/header.vue';
+  import menu from './components/menu/menu.vue';
 
   // 注册header组件,header是一个原生词，因此有冲突，需要起一个别名
   export default {
     name: 'app',
     components: {
-      'v-header': header
+      'v-header': header,
+      'v-menu': menu
     }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import "./common/css/mixin.styl"
-
-#app 
-  .tab
-    display: flex
-    width: 100%
-    height: 40px
-    line-height: 40px
-    background: #d6204b
-    border-1px(#d6204b)
-    overflow: hidden
-    .tab-item
-      flex: 1
-      text-align: center
-      & > a
-        display: block
-        text-decoration: none;
-        font-size: 14px
-        color: #ec9cae
-        &.active
-          color: #FFFFFF
-          border-1px(#d6204b)
 </style>
---
+++ gank-wxl/src/components/menu/menu.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="menu">
+         <div class="menu-list">
+            <div v-for="menu in menus" class="menu-item">
+                <router-link :to="menu.view">
+                    {{ menu.data }}
+                </router-link>
+            </div>
+         </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        name: 'v-menu',
+        data () {
+            return {
+                menus: [
+                     { view: 'welfare', data: '福利' },
+                     { view: 'android', data: '安卓' },
+                     { view: 'ios', data: 'IOS' },
+                     { view: 'web', data: '前端' },
+                     { view: 'rest', data: '休息视频' }
+                ]
+            };
+        }
+    };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.menu
+    width: 100%
+    height: 40px
+    background: #d6204b
+    .menu-list
+        display: flex
+        line-height: 40px
+        .menu-item    
+            flex: 1
+            text-align: center
+            & > a
+                display: block
+                font-size: 14px
+                &.active
+                    color: #FFFFFF
+</style>
