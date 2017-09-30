13ab54594874d2dd74be84b9d52f5dd92bfc41f3
WangXueLing <921223971@qq.com>
2017-09-30 17:39:26 +0800

    subject页面(完成)

--- Ado/src/components/header/header.vue
+++ Ado/src/components/header/header.vue
@@ -1,14 +1,9 @@
 <template>
     <div class="header" style="margin-top: -70px">
         <mt-header title="">
-            <mt-button icon="more" slot="left">电影</mt-button>
+            <mt-button icon="" slot="left">电影</mt-button>
             <mt-button icon="search" slot="right"></mt-button>
         </mt-header>
-        <mt-swipe :show-indicators="false">
-            <mt-swipe-item slot="left">1</mt-swipe-item>
-            <mt-swipe-item slot="center">2</mt-swipe-item>
-            <mt-swipe-item slot="right">3</mt-swipe-item>
-        </mt-swipe>
     </div>
 </template>
 
--- Ado/src/components/subject/subject.vue
+++ Ado/src/components/subject/subject.vue
@@ -1,21 +1,49 @@
 <template>
     <div class="subject">
-        <mt-button icon="back" slot="left" @click="back"></mt-button>
-        <mt-button icon="share" slot="right"></mt-button>
-        <div class="image"><img :src="movie.images.large"></div>
+        <div class="image">
+            <div class="button">
+                <mt-button class="back" icon="back" size="small" @click="back"></mt-button>
+
+                <mt-button class="more" icon="more" size="small"></mt-button>
+            </div>
+            <img class="poster" :src="movie.images.large">
+        </div>
         
         <div class="info">
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
-            <p class="info-content"><span class="info-title">片名</span><span class="info-text"></span> </p>
+            <p class="info-content"><span class="info-title">原名</span><span class="info-text">{{movie.original_title}}</span> </p>
+            <p class="info-content"><span class="info-title">类型</span><span class="info-text"><span v-for="genre in movie.genres">{{genre}} / </span></span> </p>
+            <p class="info-content"><span class="info-title">年代</span><span class="info-text">{{movie.year}}</span> </p>
+            <p class="info-content"><span class="info-title">想看人数</span><span class="info-text">{{movie.wish_count}}</span> </p>
+            <p class="info-content"><span class="info-title">看过人数</span><span class="info-text">{{movie.collect_count}}</span> </p>
+            <p class="info-content"><span class="info-title">评分人数</span><span class="info-text">{{movie.ratings_count}}</span> </p>
+            <p class="info-content"><span class="info-title">短评数量</span><span class="info-text">{{movie.comments_count}}</span> </p>
+            <p class="info-content"><span class="info-title">影评数量</span><span class="info-text">{{movie.reviews_count}}</span> </p>
         </div>
+
+        <div class="summary">{{movie.summary}}</div>
+
+        <p class="person-type">导演</p>
+        <div class="person">
+            <div class="person-card" v-for="director in movie.directors" :key="director.id">
+                <div class="person-card-item">
+                    <div class="per-img"><img :src="director.avatars.small"></div>
+                    <div class="per-name">{{director.name}}</div>
+                </div>
+            </div>
+        </div>
+        <p></p> </br>
+
+        <p class="person-type">主演</p>
+        <div class="person">
+            <div class="person-card" v-for="cast in movie.casts" :key="cast.id">
+                <div class="person-card-item">
+                    <div class="per-img"><img :src="cast.avatars.small"></div>
+                    <div class="per-name">{{cast.name}}</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="blank"></div>
     </div>
 </template>
 
@@ -59,8 +87,61 @@
     };</script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+.subject
+    width: 100%
+    height: 100%
+    background: #f5f5f5
     .image
         width: 100%
-        height: 40%
+        height: 350px
         background: #391505
+        .button
+            height: 50px
+            .back
+                float: left
+                margin: 15px 
+            .more
+                float: right
+                margin: 15px
+        .poster
+            width: 50%
+            height: 75%
+    .info
+        width: 100%
+        margin: 30px
+        text-align: left
+        .info-content>span
+            display: block
+        .info-text
+            font-size: 0.9rem
+            color: #888;
+    .summary
+        width: 90%
+        padding: 5%
+        text-align: left
+        background: #EEEEEE
+    .person-type
+        width: 100%
+        margin: 15px 0 -15px 25px
+        text-align: left
+    .person
+        width: 100%
+        margin: 15px 2px 0px 10px
+        display: flex
+        flex-direction: row
+        flex-wrap: wrap
+        text-align: left      
+        .person-card
+            width: 160px
+            height: 100px
+            margin: 10px 5px
+            background: #FFFFFF
+            .per-img
+                width: 75px
+                height: 100%
+                float: left
+            .per-name
+                padding: 10px
+    .blank
+        height: 150px
 </style>
